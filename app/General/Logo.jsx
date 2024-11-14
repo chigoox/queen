6ab262c6a7c0838607b001d5830eeeb4844efea7
Emmanuel Dike <117424280+chigoox@users.noreply.github.com>
2024-11-14 09:29:46 -0500

    xx

--- app/General/Logo.jsx
+++ app/General/Logo.jsx
@@ -4,7 +4,7 @@ import { Image } from "@nextui-org/image";
 function
     Logo() {
     return (
-        <Image className={'h-80 w-80 m-auto fadeIn rounded-full object-cover '} src={'/Logo.jpg'} />
+        <Image alt='logo' className={'h-80 w-80 m-auto fadeIn rounded-full object-cover '} src={'/Logo.jpg'} />
 
     )
 }

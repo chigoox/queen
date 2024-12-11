237585b9f32376f2f75fcdb9829d3f67e7edb9f6
Emmanuel Dike <117424280+chigoox@users.noreply.github.com>
2024-12-11 16:22:13 -0500

    Update find customer route.js

---
+++ app/api/FindCustomer/route.js
@@ -0,0 +1,16 @@
+import Stripe from "stripe";
+import { NextResponse } from "next/server";
+
+export const revalidate = 60
+
+export async function POST(request) {
+  let data = await request.json();
+  const { email, name, phone } = data
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
+  const customers = await stripe.customers.search( query: `name:"${name}" AND email:"${email}" AND phone:"${phone}"`);
+  return NextResponse.json(customers.data.reverse())
+}
+
+
+
+

95439676d53f695805cd9d603e64fbe15886bae4
Emmanuel Dike <117424280+chigoox@users.noreply.github.com>
2024-12-11 18:58:07 -0500

    Update route.js

--- app/api/Checkout/route.js
+++ app/api/Checkout/route.js
@@ -8,7 +8,14 @@ import Stripe from "stripe";
 export async function POST(request) {
     const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
     let data = await request.json();
-    let { customer, service, addons, name, phone, email } = data
+    let { 
+        customer = {}, 
+        service = {}, 
+        addons, 
+        name = '', 
+        phone = '', 
+        email = '' 
+    } = data;
     console.log( customer, service, addons, customerInfo, name, phone, email)
     const session = await stripe.checkout.sessions.create({
         line_items: [{

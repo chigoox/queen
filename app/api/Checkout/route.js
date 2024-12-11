d83e9f5f42413d88679370d9d4beb9df832ea759
Emmanuel Dike <117424280+chigoox@users.noreply.github.com>
2024-12-11 16:04:50 -0500

    Update route.js

--- app/api/Checkout/route.js
+++ app/api/Checkout/route.js
@@ -8,17 +8,20 @@ import Stripe from "stripe";
 export async function POST(request) {
     const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
     let data = await request.json();
-    let { cart, UID, total, cartID } = data
+    let { customer, service, addons } = data
     const session = await stripe.checkout.sessions.create({
         line_items: cart,
         mode: 'payment',
         success_url: `http://${!isDev() ? siteName?.replace(/\s/g, '').replace(/\'/g, '') + '.com' : 'localhost:3000'}/Checkout/success`,
         cancel_url: `http://${!isDev() ? siteName?.replace(/\s/g, '').replace(/\'/g, '') + '.com' : 'localhost:3000'}/Checkout/canceled`,
         metadata: {
-            uid: UID.toString(),
-            cart: JSON.stringify(cart), //for Stripe
-            cartID: cartID.toString(),
-            total: total,
+            customerID: customer?.ID,
+            customerName: customer?.name,
+            customerEmail: customer?.email,
+            customerPhone: customer?.phone,
+            serviceName: service.name,
+            servicePrice: service.price,
+            serviceTime: service.time,
             type: 'checkout'
         },
 

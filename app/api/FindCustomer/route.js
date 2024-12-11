f537b7a1a89976643788af4ab88ca8de177a3c02
Emmanuel Dike <117424280+chigoox@users.noreply.github.com>
2024-12-11 16:27:06 -0500

    Update route.js

--- app/api/FindCustomer/route.js
+++ app/api/FindCustomer/route.js
@@ -7,6 +7,7 @@ export async function POST(request) {
   let data = await request.json();
   const { email, name, phone } = data
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
+  console.log(email)
   const customers = await stripe.customers.search( query: `name:"${name}" AND email:"${email}" AND phone:"${phone}"`);
   return NextResponse.json(customers.data.reverse())
 }

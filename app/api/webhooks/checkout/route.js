da6afae59024b834f3669d63b392b5c49266e644
Emmanuel Dike <117424280+chigoox@users.noreply.github.com>
2024-12-15 17:44:30 -0500

    Update route.js

--- app/api/webhooks/checkout/route.js
+++ app/api/webhooks/checkout/route.js
@@ -37,7 +37,7 @@ export async function POST(request) {
 
 
         const ORDERID = 0
-        await addToDoc('Orders', ORDERID, order)
+        await addToDoc('Orders', ORDERID, 'order')
 
         const ORDERS = await FetchTheseDocs('Orders', 'id', '==', ORDERID, 'id') //Object.values(JSON.parse(fullCart))
 

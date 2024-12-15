02ed16e4428c965ba4e3912b1dabe47b11a0cf81
Emmanuel Dike <117424280+chigoox@users.noreply.github.com>
2024-12-15 13:16:22 -0500

    Update route.js

--- app/api/Checkout/route.js
+++ app/api/Checkout/route.js
@@ -43,7 +43,7 @@ export async function POST(request) {
             serviceName: service?.name,
             servicePrice: service?.price,
             serviceTime: service?.time,
-            type: 'checkout'
+            type: 'checkout x'
         },
 
     })

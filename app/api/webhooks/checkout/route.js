0cdb1595b9a2cc00fb36b4eeba1dfd65f26057cf
Emmanuel Dike <117424280+chigoox@users.noreply.github.com>
2024-12-17 08:51:55 -0500

    x

--- app/api/webhooks/checkout/route.js
+++ app/api/webhooks/checkout/route.js
@@ -1,4 +1,4 @@
-import { FetchTheseDocs, addToDatabase, addToDoc} from "@/app/myCodes/Database";
+import { FetchTheseDocs, addToDatabase, addToDoc } from "@/app/myCodes/Database";
 import { format } from "date-fns";
 import { serverTimestamp } from "firebase/firestore";
 import Cors from "micro-cors";
@@ -23,31 +23,33 @@ export async function POST(request) {
 
     if (event.type === "checkout.session.completed") {
       console.log(event.data.object.metadata)
-     
+      const data = JSON.parse(event.data.object.metadata)
+      const { appointmentDate, appointmentTime, service, addons, customerName, customerEmail, customerPhone } = data
       //OrderConfirmationMail()
-       
-       
-      
-        const appointment = {
-          
-       
-          dateServer: serverTimestamp(),
-          dateReal: new Date().toLocaleString()
-        }
+      console.log(appointmentDate, appointmentTime, service, addons, customerName, customerEmail, customerPhone)
 
 
-        const ORDERID = 0
-        await addToDoc('Orders', ORDERID, 'order')
 
-        const ORDERS = await FetchTheseDocs('Orders', 'id', '==', ORDERID, 'id') //Object.values(JSON.parse(fullCart))
+      const appointment = {
 
-        if (ORDERS[0]?.id == ORDERID) {
 
-          await updateDatabaseItem('Admin', 'Orders', 'orderID', orderID + 1)
-        }
-
-        await addToDatabase('User', 'uid', 'currentOrder', ORDERID)
+        dateServer: serverTimestamp(),
+        dateReal: new Date().toLocaleString()
       }
+
+
+      const ORDERID = 0
+      await addToDoc('Orders', ORDERID, 'order')
+
+      const ORDERS = await FetchTheseDocs('Orders', 'id', '==', ORDERID, 'id') //Object.values(JSON.parse(fullCart))
+
+      if (ORDERS[0]?.id == ORDERID) {
+
+        await updateDatabaseItem('Admin', 'Orders', 'orderID', orderID + 1)
+      }
+
+      await addToDatabase('User', 'uid', 'currentOrder', ORDERID)
+    }
 
 
     return NextResponse.json({ result: event, ok: true });

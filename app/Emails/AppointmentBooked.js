a4a28a2e499fbd141b5782735b9e3d3e1ab8b9d7
Emmanuel Dike <117424280+chigoox@users.noreply.github.com>
2024-12-19 21:49:06 +0000

    update email

--- app/Emails/AppointmentBooked.js
+++ app/Emails/AppointmentBooked.js
@@ -2,15 +2,27 @@ import React from 'react';
 import { Button, Html } from "@react-email/components";
 
 export const AppointmentBooked = ({ cusomterInfo,  service,  addons,apointmentTime, apointmentDate }) => {
-
+  console.log( cusomterInfo,  service,  addons,apointmentTime, apointmentDate)
+  const totalPrice = service?.reduce((sum, service) => sum + service.price, 0) || 0 
   return(
     <Html>
-      <Button
-        href="https://example.com"
-        style={{ background: "#000", color: "#fff", padding: "12px 20px" }}
-      >
-        Click me
-      </Button>
+      <Body style={main}>
+        <Container style={container}>
+          <Text style={paragraph}>Hi {cusomterInfo?.customerName},</Text>
+          <Text style={paragraph}>
+            Your appointment has been confirmed. Here are the details:
+          </Text>
+          <Text style={paragraph}><strong>Appointment Time:</strong> {apointmentTime} {apointmentDate}</Text>
+          <Text style={paragraph}><strong>Services:</strong></Text>
+          <ul style={listStyle}>
+          {service.name} - ${service.price.toFixed(2)}
+          </ul>
+          <Text style={paragraph}><strong>Total Price:</strong> ${totalPrice.toFixed(2)}</Text>
+          <Text style={paragraph}>
+            Thank you for booking with us!
+          </Text>
+        </Container>
+      </Body>
     </Html>
   )
 }

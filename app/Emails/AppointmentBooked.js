df41d86aa770d705ebd022829bee5510ca2dfa70
Emmanuel Dike <117424280+chigoox@users.noreply.github.com>
2024-12-19 21:26:53 +0000

    x

--- app/Emails/AppointmentBooked.js
+++ app/Emails/AppointmentBooked.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { Button, Html } from "@react-email/components";
 
 export const AppointmentBooked = ({ cusomterInfo,  service,  addons,apointmentTime, apointmentDate }) => {
 
   return(
-    <div>{apointmentTime}</div>
+    <Html>
+      <Button
+        href="https://example.com"
+        style={{ background: "#000", color: "#fff", padding: "12px 20px" }}
+      >
+        Click me
+      </Button>
+    </Html>
   )
 }

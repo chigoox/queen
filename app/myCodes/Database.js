074763ca5471c235c6e3d08e8a182e52e60a8736
Emmanuel Dike <117424280+chigoox@users.noreply.github.com>
2024-12-20 04:23:49 -0500

    x

--- app/myCodes/Database.js
+++ app/myCodes/Database.js
@@ -24,7 +24,7 @@ export const watchDocument = async (collection, Doc, setter) => {
 }
 
 export async function addToDoc(collection, Doc, data) {
-    console.log('from coc',data)
+    console.log('from ccoc',data)
     if (Doc) {
         try {
             await setDoc(doc(DATABASE, collection, Doc),

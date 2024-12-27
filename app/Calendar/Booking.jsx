7f19e6ac7c8c1ee83dba1168f5bbb92e34d7a961
Emmanuel Dike <117424280+chigoox@users.noreply.github.com>
2024-12-27 00:13:16 +0000

    x

--- app/Calendar/Booking.jsx
+++ app/Calendar/Booking.jsx
@@ -224,20 +224,18 @@ const bookID = getRandTN(10)
              redirect: 'follow',
             body: JSON.stringify({
                 price: bookingInfo?.metadata?.price || 0,// ORIGINAL (bookingInfo?.price * (bookingInfo.bundle ? 1 : 0.50) * bookingInfo.bundle ? 4 : 1) - bookingInfo.bundle ? 50 : 0, //if bundled( price * 4 - 50) else (price/2)
-                name: bookingInfo?.customer.name,
-                email: bookingInfo?.customer.email,
-                phone: bookingInfo?.customer.phone,
+                name: bookingInfo?.customer.name || null,
+                email: bookingInfo?.customer.email || null,
+                phone: bookingInfo?.customer.phone || null,
                 addons: JSON.stringify(bookingInfo.addons || {}),
                 customer: customerID || null,
                 service: JSON.stringify(bookingInfo.service || {}),
-                apointmentDate: bookingInfo?.apointment,
-                apointmentTime: bookingInfo?.time12,
-                ownerID: OWNER?.uid,
-                OwnerUserName: OWNER?.userName,
-                bookID: bookID
-
-
-              
+                apointmentDate: bookingInfo?.apointment || null,
+                apointmentTime: bookingInfo?.time12 || null,
+                ownerID: OWNER?.uid || null,
+                OwnerUserName: OWNER?.userName || null,
+                bookID: bookID || null,
+                apointmentID: apointmentID || null,
             })
         })
 

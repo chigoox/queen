6942f6d59180d5e4ddc064c70a6f97dc38587a5f
Emmanuel Dike <117424280+chigoox@users.noreply.github.com>
2024-12-28 15:43:28 +0000

    Refactor checkout session creation to handle empty customer case and streamline code structure

--- app/api/Checkout/route.js
+++ app/api/Checkout/route.js
@@ -20,39 +20,74 @@ export async function POST(request) {
         OwnerUserName = ''
     } = data;
 
-    const session = await stripe.checkout.sessions.create({
-        line_items: [
-            {
-                price_data: {
-                    currency: "usd",
-                    product_data: {
-                        name: "Crowned Brows & Lashes Deposit"
+    if (customer === '') {
+        const session = await stripe.checkout.sessions.create({
+            line_items: [
+                {
+                    price_data: {
+                        currency: "usd",
+                        product_data: {
+                            name: "Crowned Brows & Lashes Deposit"
+                        },
+                        unit_amount: 2500
                     },
-                    unit_amount: 2500
-                },
-                quantity: 1
-            }
-        ],
-        customer: customer || null,
-        mode: 'payment',
-        success_url: `https://${!isDev() ? 'crownedbl.vercel.app' : 'localhost:3000'}/Checkout/Success/${ownerID}`,
-        cancel_url: `https://${!isDev() ? 'crownedbl.vercel.app' : 'localhost:3000'}/Checkout/Cancelled/${ownerID}`,
-        metadata: {
-            customerID: customer,
-            customerName: name,
-            customerEmail: email,
-            customerPhone: phone,
-            service: service,
-            addons: addons,
-            apointmentDate: apointmentDate,
-            apointmentTime: apointmentTime,
-            type: 'checkout',
-            ownerID:ownerID,
-            OwnerUserName:OwnerUserName,
-            apointmentID: apointmentID,
-        },
-
-    })
+                    quantity: 1
+                }
+            ],
+            mode: 'payment',
+            success_url: `https://${!isDev() ? 'crownedbl.vercel.app' : 'localhost:3000'}/Checkout/Success/${ownerID}`,
+            cancel_url: `https://${!isDev() ? 'crownedbl.vercel.app' : 'localhost:3000'}/Checkout/Cancelled/${ownerID}`,
+            metadata: {
+                customerID: customer,
+                customerName: name,
+                customerEmail: email,
+                customerPhone: phone,
+                service: service,
+                addons: addons,
+                apointmentDate: apointmentDate,
+                apointmentTime: apointmentTime,
+                type: 'checkout',
+                ownerID:ownerID,
+                OwnerUserName:OwnerUserName,
+                apointmentID: apointmentID,
+            },
+    
+        })
+    }else{
+        const session = await stripe.checkout.sessions.create({
+            line_items: [
+                {
+                    price_data: {
+                        currency: "usd",
+                        product_data: {
+                            name: "Crowned Brows & Lashes Deposit"
+                        },
+                        unit_amount: 2500
+                    },
+                    quantity: 1
+                }
+            ],
+            customer: customer || null,
+            mode: 'payment',
+            success_url: `https://${!isDev() ? 'crownedbl.vercel.app' : 'localhost:3000'}/Checkout/Success/${ownerID}`,
+            cancel_url: `https://${!isDev() ? 'crownedbl.vercel.app' : 'localhost:3000'}/Checkout/Cancelled/${ownerID}`,
+            metadata: {
+                customerID: customer,
+                customerName: name,
+                customerEmail: email,
+                customerPhone: phone,
+                service: service,
+                addons: addons,
+                apointmentDate: apointmentDate,
+                apointmentTime: apointmentTime,
+                type: 'checkout',
+                ownerID:ownerID,
+                OwnerUserName:OwnerUserName,
+                apointmentID: apointmentID,
+            },
+    
+        })
+    }
     console.log(session.url)
     return NextResponse.json(session.url)
 }

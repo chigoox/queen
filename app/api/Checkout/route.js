e3665786ca2503905795813170f5fa9fd602dc26
Emmanuel Dike <117424280+chigoox@users.noreply.github.com>
2024-12-28 15:49:56 +0000

    x

--- app/api/Checkout/route.js
+++ app/api/Checkout/route.js
@@ -19,7 +19,7 @@ export async function POST(request) {
         apointmentTime = '',
         OwnerUserName = ''
     } = data;
-
+console.log(customer)
     if (customer === '') {
         const session = await stripe.checkout.sessions.create({
             line_items: [
